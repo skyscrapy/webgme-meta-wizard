855dfc5771e62300c36acc6c97168672ee0e8aa4
SuperStevenZ <zzuohong@gmail.com>
2015-11-02 23:55:10 -0600

    stopped by creatBranch

--- src/app/js/controller.js
+++ src/app/js/controller.js
@@ -1,16 +1,58 @@
-var app = angular.module('webgme_wizard', []);
-		app.controller('FormCtrl', function ($scope, $http) {
-			$scope.data = {
-				projectName: "default",
-				branchName: "default",
-				manageMeta: "Add",
-				manageAttr: "Add",
-				addAttrName: "default",
-				addAttrVal: "default",
-				editAttrVal: "default",
+var app = angular.module('webgmeWizard', []);
+
+app.controller('projectCtrl', function ($scope) {
+	$scope.submitForm = function() {
+		/* create project from seed*/
+		var client = new GME.classes.Client(GME.gmeConfig);
+
+		client.connectToDatabase(function (err) {
+			if (err) {
+				console.log(err);
+			}
+			var parameters = {
+				seedName: 'guest+seed',
+				seedBranch: 'master',
+				projectName: $scope.projectName
 			};
-			$scope.submitForm = function() {
-				console.log("sending data");
-				/* do CRUD operation */
-			};
+			console.log(parameters);
+			client.seedProject(parameters, function (err, result) {
+				if (err) {
+					console.log(err);
+				}
+			});
 		});
+	};
+});
+
+app.controller('branchCtrl', function ($scope) {
+	/* list all the projects and branches*/
+	var client = new GME.classes.Client(GME.gmeConfig);
+	console.log(client);
+
+	client.connectToDatabase(function (err) {
+		if (err) {
+			console.log(err);
+		}
+		client.getProjectsAndBranches(false, function (err, myProjects) {
+			if (err) {
+				console.log(err);
+			}
+			$scope.projects = myProjects;
+		});
+	});
+
+	$scope.submitForm = function() {
+		/* create project from seed*/
+		client.connectToDatabase(function (err) {
+			if (err) {
+				console.log(err);
+			}
+			console.log($scope.selectedProject);
+		});
+	};
+});
+
+app.controller('attributesCtrl', function ($scope) {
+	/* list all the projects and branches*/
+
+});
